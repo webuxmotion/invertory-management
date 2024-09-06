c0d95fc228d35ab6039342ef52dac4055affd74e
webuxmotion <pereverziev.andrii@gmail.com>
2024-09-07 00:27:24 +0300

    Start Sidebar

---
+++ src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Menu } from 'lucide-react';
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">EDSTOCK</h1>
+        <button 
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue"
+          onClick={() => {}}
+        >
+          <Menu className='w-4 h-4' />
+        </button>
+      </div>
+
+      {/* LINKS */}
+      <div className="flex-grow mt-8">
+        links
+      </div>
+
+      {/* FOOTER */}
+      <div>
+        <p className='text-center text-xs text-gray-500'>&copy; 2024 Edstock</p>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar;
