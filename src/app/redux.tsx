9a893415d11a1b4b0fd108fd097152172f325fb4
webuxmotion <pereverziev.andrii@gmail.com>
2024-09-07 15:08:27 +0300

    _key

--- src/app/redux.tsx
+++ src/app/redux.tsx
@@ -27,12 +27,15 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 const createNoopStorage = () => {
   return {
     getItem(_key: any) {
+      console.log(_key);
       return Promise.resolve(null);
     },
     setItem(_key: any, value: any) {
+      console.log(_key);
       return Promise.resolve(value);
     },
     removeItem(_key: any) {
+      console.log(_key);
       return Promise.resolve();
     },
   };
