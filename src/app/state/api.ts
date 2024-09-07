c0e8496dd47643fd1d64c3b366ceca5d3c66ab7f
webuxmotion <pereverziev.andrii@gmail.com>
2024-09-07 15:22:40 +0300

    React version

--- src/app/state/api.ts
+++ src/app/state/api.ts
@@ -1,10 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-    reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({})
-})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => {
+    console.log({ build });
+
+    return {};
+  },
+});
 
 export const {} = api;
